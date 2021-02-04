0ed1d95c9e73aa5c84830d0f8d9c3e197c2ea755
Deepak Mahajan <deepakmahajan269@gmail.com>
2021-02-04 17:03:13 +0530

    Instagra-Clone

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,21 +63,14 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
   config.action_mailer.default_url_options = { host: 'instaagram-clone-2020.herokuapp.com' }
   config.action_mailer.raise_delivery_errors = true
-
-  config.action_mailer.perform_caching = false
-
   config.action_mailer.perform_deliveries = true
-
   config.action_mailer.delivery_method = :smtp
-
-  config.action_mailer.default_url_options = { host: ENV['MAIL_HOST'] }
-
   config.action_mailer.smtp_settings = {
     user_name:      ENV['SENDMAIL_USERNAME'],
     password:       ENV['SENDMAIL_PASSWORD'],
-    domain:         ENV['MAIL_HOST'],
     address:       'smtp.gmail.com',
     port:          '587',
+    domain: "instaagram-clone-2020.herokuapp.com"
     authentication: :plain,
     enable_starttls_auto: true
   }
