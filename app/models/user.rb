8ac6eac244b86ba88f6db8f92834e2288a9527f2
Deepak Mahajan <deepakmahajan269@gmail.com>
2021-02-04 16:39:50 +0530

    Instagram-Clone

--- app/models/user.rb
+++ app/models/user.rb
@@ -34,6 +34,7 @@ class User < ApplicationRecord
       user = self.where(email: auth.info.email).first
       user.provider = auth.provider
       user.uid = auth.uid
+      user.url = auth.info.image
     else
       where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
         user.email = auth.info.email
