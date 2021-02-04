5876bfacf4a381132aa1b3d7d27c82cd7b7d8f58
Deepak Mahajan <deepakmahajan269@gmail.com>
2021-02-04 14:58:20 +0530

    Instagram-Clone

--- app/javascript/stylesheets/tailwind.config.js
+++ app/javascript/stylesheets/tailwind.config.js
@@ -6,7 +6,7 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     screens: {
-      sm: '425px',
+      sm: '320px',
       md: '768px',
       lg: '1024px',
       xl: '1280px',
